bd4faf4e15cfa463a27093f6e03d29990c08a3aa
Kelly <kelly.hao.7@hotmail.com>
2018-10-12 16:56:02 -0400

    csv read

--- source/input_output.py
+++ source/input_output.py
@@ -8,7 +8,21 @@ class SeqHandler:
     def read(self):
         print("Running")
         # TODO read and format the CSV sequence file into array of [psi,theta,phi,x,y,z,time] vectors
-        return self.sequence
+        import csv
+        with open('Maze Solution.csv', 'r') as csvfile:
+            csv_reader = csv.reader(csvfile, delimiter=',')
+            line_count=0
+            for row in csv_reader:
+                if line_count==0:
+                    print (f'Array is formatted as [psi,theta,phi,x,y,z,time] vectors')
+                    line_count +=1
+                else:
+                    data=[]
+                    for row in csv_reader:
+                        data.append(row)
+            print(data[1:])
+        # I wrote this, pls delete or modify if it's not what how you want it
+
      
     def write(self):
         # TODO write a nice array for the arduino to read. Probably txt or CSV
