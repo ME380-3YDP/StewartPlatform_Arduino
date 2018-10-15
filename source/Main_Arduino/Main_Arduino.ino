c4ec855d11841b28120d035addbd82e7012885aa
Stefan <stefanm1010@gmail.com>
2018-10-15 19:35:35 -0400

    We're moving to python
    Change of plans, we are going to write everything except the PWM stuff in python because it make things easy. The user commands and everything will be handled here and the results sent to the arduino over serial as servo angle values.

--- source/Main_Arduino/Main_Arduino.ino
+++ source/Main_Arduino/Main_Arduino.ino
@@ -1,30 +1,3 @@
-class baked //precomputed from python module unless we want to hook them together somehow
-{
-  public:
-  int mazeSolution=0;//hardcoded 2D array of lenghts and timings for mazesolution. Might be a better way to do this using std::Vectors
-  int wave=0; //do the wave
-  int tiltX=0;// manual mode positions
-  int tiltY=0;// manual mode positions
-};
-
-class userInterface
-{public:
-
-  bool errorHandler(int e){
-    //TODO output the error to serial
-    };
-
-  bool stateWriter(char ){
-    //TODO print the state of the system (positions, ect) to Serial for debugging
-    };
-  void inputHandler(int key){
-    //determine what to do when the user presses a certain key, see example code
-    //TODO handle commands to run the solution, reset to the default position, manual yaw and pitch
-    };
-  };
-
-baked motions; //new pre-baked motions object
-
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
@@ -32,15 +5,16 @@ Serial.begin(9600);
 
 
 void loop() {
-  bool run=0;
-  //(pseudocode) Serial.onRecieve= UI.inputHandler(serial.recieved)
-  //wait for "run" command in this loop
-  if (run) mazeSolve(motions.mazeSolution); //run with the main maze solution
-}
+if (Serial.available() > 0) {
+                // read the incoming byte:
+                int data = Serial.read();
 
-void motorAngles(int lengthVector){
-  //TODO compute the servo angles for each servo based on the required length. Lengths are defined with 0 being fully retracted (deltaL).
-  //Audrey, I think this one is yours
+                // say what you got:
+                Serial.print("I received: ");
+                Serial.println(data, DEC);
+                //wait for us to get 6 values
+                servoInterface(data) #tell the servos to move
+        }
 }
 
 void servoInterface(int anglesVector){
@@ -48,19 +22,4 @@ void servoInterface(int anglesVector){
   //Kelly this is all you
 }
 
-bool mazeSolve(int lengthVector){
-  userInterface UI; //define a new userInterface object
-  for(obj = var in lengthVector) //TODO build proper array iterator
-  {
-    timeDelay=obj.time; //get time from the lengthVector
-    angles=motorAngles(lengthVector) //compute angles
-    servoInterface(angles) //move the servoes
-    delay(timeDelay)//wait until the end of the move
-    UI.stateWriter(lengthVector) //output our expected lenghts or whatever
-  }
-  return 1;
-  if (error){ //general form for any errors we have
-    UI.errorHandler(error); 
-    return 0;
-  }
-}
+
--- source/input_output.py
+++ source/input_output.py
@@ -1,4 +1,6 @@
 from csv import writer
+import serial
+import struct
 
 class SeqHandler:
     def __init__(self,path):
@@ -13,3 +15,16 @@ class SeqHandler:
     def write(self):
         # TODO write a nice array for the arduino to read. Probably txt or CSV
         return 1
+class Arduino:
+    def __init__(self):
+        self.serial=serial.Serial('/dev/ttyACM1', 9600)
+
+    def read(self):
+        data=self.serial.readline()
+        return data
+
+    def write(data):
+        if type(data)==int:
+            self.serial.write(struct.pack('>B', data)) # this works for all integers, tested it
+            response=int(testSer.readline())
+        return response
