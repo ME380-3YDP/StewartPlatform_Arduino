8040eec448d259684cf81dc86afc6669d5ca2a34
Kelly <kelly.hao.7@hotmail.com>
2018-10-30 13:29:12 -0400

    computeanglescode (incomplete)

--- source/test for angles.py
+++ source/test for angles.py
@@ -3,26 +3,22 @@
 import numpy as np
 import math
 
-length = [1, 2, 3, 4, 5, 6] #to be removed with def and real shiz once code is complete
+length = [0.003, 0.002, 0.003, 0.003, 0.003, 0.003] #to be removed with def and real shiz once code is complete
 angles=[]
-a = 0.035 #length of crank, subject to change
-b = 0.0075 #length of con rod, subject to change
+a = 0.0035 #length of crank, subject to change. This is measured with the hole spacing center to center (last clearance hole to the hole that connects to the conrod)
+b = 0.0090 #length of con rod, subject to change (center to center distance) 
 c = [a+b, a+b, a+b, a+b, a+b, a+b] #starting length when syringe plunger fully enclosed (retracted pos'n)
 print(length)
 c_length =[]
-c_length =np.subtract(c, length) #so we can use trig functions
+c_length =np.subtract(c, length) #length to use for cosine law
 print(c_length)
-inverseanglemath=[]
 inverseangle=[]
-anglecalculation=[]
 for i in c_length:
-    inverseanglemath= (-(b**2 - a**2 - (i)**2)/(2*a*(i))
-
-    for j in c_length:
-        anglecalculation= np.arccos(inverseanglemath(i))
-    #inverseangle.append(inverseanglemath)
-    #print(inverseangle)
-    angles.append(anglecalculation)
+    inverseanglemath= -(-b**2 - a**2 + (i)**2)/(2*a*b)
+    print(inverseanglemath)
+    calculation= math.acos(inverseanglemath)
+    angles.append(calculation)
+    #TODO convert to PWN 
 print(angles)
         
         #return angles #should be a tuple of 6 servo angles between 150 and 280 (a0,a1...,a5)
